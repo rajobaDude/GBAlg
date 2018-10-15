8cbfd8c7347cf3738cf4433a0ff8c3fdba2bae1e
Ivan Anurov <rajoba@yandex.ru>
2018-10-15 21:07:20 +0300

    Исправление по домашке

--- L2 HW/src/ArrayTest.java
+++ L2 HW/src/ArrayTest.java
@@ -22,22 +22,38 @@ public class ArrayTest {
         Array arr_2 = new ArrayImpl(arr_1.getMaxSize());
         Array arr_3 = new ArrayImpl(arr_1.getMaxSize());
 
-        arr_2.setData(arr_1.getData());
-        arr_3.setData(arr_1.getData());
+        arr_2.setData(Arrays.copyOf(arr_1.getData(), arr_1.getSize()));
+        arr_3.setData(Arrays.copyOf(arr_1.getData(), arr_1.getSize()));
+
+        System.out.println(arr_1.get(0) + " " + arr_1.get(5000) + " " + arr_1.get(99999));
+        System.out.println(arr_2.get(0) + " " + arr_2.get(5000) + " " + arr_2.get(99999));
+        System.out.println(arr_3.get(0) + " " + arr_3.get(5000) + " " + arr_3.get(99999));
 
         System.out.println("Время работы пузырька: ");
         long start1 = System.nanoTime();
         SortingClass.sortBubble(arr_1.getData());
         System.out.println(TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - start1));
 
+        System.out.println(arr_1.get(0) + " " + arr_1.get(5000) + " " + arr_1.get(99999));
+        System.out.println(arr_2.get(0) + " " + arr_2.get(5000) + " " + arr_2.get(99999));
+        System.out.println(arr_3.get(0) + " " + arr_3.get(5000) + " " + arr_3.get(99999));
+
         System.out.println("Время работы выбора: ");
         long start2 = System.nanoTime();
         SortingClass.sortSelect(arr_2.getData());
         System.out.println(TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - start2));
 
+        System.out.println(arr_1.get(0) + " " + arr_1.get(5000) + " " + arr_1.get(99999));
+        System.out.println(arr_2.get(0) + " " + arr_2.get(5000) + " " + arr_2.get(99999));
+        System.out.println(arr_3.get(0) + " " + arr_3.get(5000) + " " + arr_3.get(99999));
+
         System.out.println("Время работы вставки: ");
         long start3 = System.nanoTime();
         SortingClass.sortInsert(arr_3.getData());
         System.out.println(TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - start3));
+
+        System.out.println(arr_1.get(0) + " " + arr_1.get(5000) + " " + arr_1.get(99999));
+        System.out.println(arr_2.get(0) + " " + arr_2.get(5000) + " " + arr_2.get(99999));
+        System.out.println(arr_3.get(0) + " " + arr_3.get(5000) + " " + arr_3.get(99999));
     }
 }
